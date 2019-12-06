7d4b3872e0f8fcc6d33527d8bd8c4f93416389ea
chendongdong <chendongdong@zhidaoauto.com>
2019-12-06 18:31:59 +0800

    xxxx

--- src/main/java/com/seven/ticket/manager/RailCookieManager.java
+++ src/main/java/com/seven/ticket/manager/RailCookieManager.java
@@ -1,10 +1,15 @@
 package com.seven.ticket.manager;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.util.Cookie;
 import com.seven.ticket.config.Constants;
 import com.seven.ticket.request.OkHttpRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -20,6 +25,32 @@ import java.util.Set;
 @Slf4j
 public class RailCookieManager {
 
+   public static void init2(){
+       String url="https://kyfw.12306.cn/otn/HttpZF/logdevice?algID=xzQIvaEleQ&hashCode=35nup1m6FOVXyoTJVLebRTqnIxmscYo_2mJCvkGS9IU&FMQw=0&q4f3=zh-CN&VPIf=1&custID=133&VEek=unknown&dzuS=0&yD16=0&EOQP=8946ffc6b619470a790de1a7b7d6683f&lEnu=2886994437&jp76=52d67b2a5aa5e031084733d5006cc664&hAqN=Win32&platform=WEB&ks0Q=d22ca0b81584fbea62237b14bd04c866&TeRS=824x1536&tOHY=24xx864x1536&Fvje=i1l1o1s1&q5aJ=-8&wNLf=99115dfb07133750ba677d055874de87&0aew=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/78.0.3904.97%20Safari/537.36&E3gR=42f8c6830d6f3907fcbc9dd5fc505f3d&timestamp="+System.currentTimeMillis();
+       HttpGet httpGet = OkHttpRequest
+                .setRequestHeader(new HttpGet(url), true, false, false);
+        try {
+
+            log.info("获取RAIL COOKIE信息");
+            CloseableHttpResponse response= OkHttpRequest.getSession().execute(httpGet);
+            // 设置到session的cookie中
+            String responseText = OkHttpRequest.responseToString(response);
+            JSONObject jsonObject = railCookie(responseText);
+            String railExpiration = jsonObject.get("exp").toString();
+            String railDeviceId = jsonObject.get("dfp").toString();
+            OkHttpRequest.setCookie("RAIL_EXPIRATION", railExpiration);
+            OkHttpRequest.setCookie("RAIL_DEVICEID", railDeviceId);
+        } catch (IOException e) {
+            log.info("获取RAIL COOKIE信息失败:{}",ExceptionUtils.getRootCause(e));
+        }
+    }
+    private static JSONObject railCookie(String resposneText) {
+        String startIdxStr = "{";
+        String endIdxStr = "}";
+        resposneText = resposneText.substring(resposneText.indexOf(startIdxStr), resposneText.indexOf(endIdxStr) + 1);
+        JSONObject jsonObject = JSON.parseObject(resposneText);
+        return jsonObject;
+    }
     public static void init(){
         log.info("获取RAIL COOKIE信息");
         WebClient webclient = new WebClient();
@@ -50,17 +81,19 @@ public class RailCookieManager {
                 Set<Cookie> cookies= webclient.getCookieManager().getCookies();
                 for (Cookie cookie : cookies) {
                     if ("RAIL_EXPIRATION".equals(cookie.getName())) {
-                        OkHttpRequest.setCookie("RAIL_EXPIRATION", cookie.getValue());
                         log.info("获取 RAIL_EXPIRATION 信息 :{}",cookie.getValue());
                         isOk=true;
                     }
                     if ("RAIL_DEVICEID".equals(cookie.getName())) {
-                        OkHttpRequest.setCookie("RAIL_DEVICEID", cookie.getValue());
                         log.info("获取 RAIL_DEVICEID 信息 :{}",cookie.getValue());
                         isOk=true;
                     }
                 }
                 Thread.sleep(500);
+            }
+            Set<Cookie> cookies= webclient.getCookieManager().getCookies();
+            for (Cookie cookie : cookies) {
+                OkHttpRequest.setCookie(cookie.getName(), cookie.getValue());
             }
         } catch (MalformedURLException e) {
         } catch (IOException e) {
